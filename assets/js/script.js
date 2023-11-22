630c39aa19319ee1c923843c1f5eeab849b21e76
Edison <aalevenegass@gmail.com>
2023-11-22 11:24:11 -0300

    Añadiendo sentencias try catch para posibles errores

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,29 @@
 let conversorInput = document.getElementById("input-1");
 let conversorResultado = document.getElementById("resultado");
 let botonConversor = document.getElementById("btn-1");
+let alertId1 = document.getElementById("alert-1");
+let alertId2 = document.getElementById("alert-2");
 let dolarURL = "https://www.mindicador.cl/api/dolar";
 let euroURL = "https://www.mindicador.cl/api/euro";
 
 //función que hace una petición a una API, y lo transforma a objeto
 async function dolarConversor() {
-  const respuesta = await fetch(dolarURL);
-  const data = await respuesta.json();
-  return data;
+  try {
+    const respuesta = await fetch(dolarURL);
+    const data = await respuesta.json();
+    return data;
+  } catch (error) {
+    conversorResultado.textContent = `Algo salió mal :( ${error.message} ;`;
+  }
 }
 async function euroConversor() {
-  const respuesta = await fetch(euroURL);
-  const data = await respuesta.json();
-  return data;
+  try {
+    const respuesta = await fetch(euroURL);
+    const data = await respuesta.json();
+    return data;
+  } catch (error) {
+    conversorResultado.textContent = `Algo salió mal :( ${error.message} ;`;
+  }
 }
 
 //función que recibe una promesa desde API monedas y calcula en base al valor recibido de un input
@@ -37,8 +47,6 @@ async function renderEuro() {
 //función que muestra la conversión de moneda de peso a dolar
 botonConversor.addEventListener("click", () => {
   let select = document.getElementById("select-1");
-  let alertId1 = document.getElementById("alert-1");
-  let alertId2 = document.getElementById("alert-2");
   if (select.value === "coin") {
     alertId2.textContent = "Selecciona una moneda";
   } else {
@@ -47,7 +55,7 @@ botonConversor.addEventListener("click", () => {
   if (select.value === "dolar") {
     if (conversorInput.value === "")
       (alertId1.textContent = "ingresa un monto"),
-        (conversorResultado.textContent = "");
+        (conversorResultado.textContent = "...");
     else {
       renderDolar();
       alertId1.textContent = "";
@@ -55,7 +63,7 @@ botonConversor.addEventListener("click", () => {
   } else if (select.value === "euro") {
     if (conversorInput.value === "")
       (alertId1.textContent = "ingresa un monto"),
-        (conversorResultado.textContent = "");
+        (conversorResultado.textContent = "...");
     else {
       renderEuro();
       alertId1.textContent = "";
